d4218b0ec2cdb2ebe6ea10acc665e19334e3681b
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-06-15 15:06:14 -0400

    shapes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 let isModalOpen = false;
 let isDarkTheme = false;
+const scaleFactor = 1/20
 
 function contact(event) {
     event.preventDefault()
@@ -57,5 +58,17 @@ function toggleContrast() {
     }
     else {
         document.body.classList.remove('dark-theme')
+    }
+}
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 === 0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px)`
     }
 }
